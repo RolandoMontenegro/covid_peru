0c52ac5c639bf34e6aa50c76d6b1ad1445ab2bf4
Rolando <rmontenegro@lamolina.edu.pe>
2020-04-24 00:02:29 -0500

    Para comparar

--- script1.R
+++ script1.R
@@ -3,4 +3,5 @@ head(iris)
 library(dplyr)
 library(ggplot2)
 iris %>% 
-  ggplot(aes(x=Sepal.Length,y=Sepal.Width,col=Species))+geom_point()
+  ggplot(aes(x=Sepal.Length,y=Sepal.Width,col=Species))+geom_point()+
+  geom_smooth()
